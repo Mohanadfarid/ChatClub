309a0df6fedaf5b7c3c0afe95c5684b5759a507d
Mohanad Farid <106895779+Mohanadfarid@users.noreply.github.com>
2025-01-07 20:16:53 +0200

    added basic ui for active chat ui

--- src/views/chat/ActiveChatContainer.vue
+++ src/views/chat/ActiveChatContainer.vue
@@ -1,9 +1,37 @@
 <script lang="ts" setup>
-  //
+//
 </script>
 
-
 <template>
-  <div>this is the active chat contianer</div>
+  <div class="active-chat-container mb-2 border">
+    <v-toolbar title="Application">
+      <template v-slot:title>
+        <div class="d-flex align-center">
+          <!-- this arrow is used to go back to the list of chats  -->
+           <!-- to do handle this arrow with a v-if -->
+          <!-- <v-icon>mdi-arrow-left</v-icon> -->
+
+          <v-avatar class="me-2">
+            <img  src="https://picsum.photos/200/300" alt="" />
+          </v-avatar>
+
+          <div>
+            <p>active chat title</p>
+            <p class="text-subtitle-2">2 memebers</p>
+          </div>
+
+        </div>
+      </template>
+
+      <template v-slot:append>
+        <v-icon class="me-2">mdi-dots-vertical</v-icon>
+      </template>
+    </v-toolbar>
+  </div>
 </template>
 
+<style>
+.active-chat-container {
+  height: calc(100% - 60px);
+}
+</style>
