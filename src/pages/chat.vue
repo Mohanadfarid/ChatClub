e995ac6edc2eb23dfb25dc9651d3fee2b64de5c2
Mohanad Farid <106895779+Mohanadfarid@users.noreply.github.com>
2025-01-14 23:35:54 +0200

    fixed chat responssivness

--- src/pages/chat.vue
+++ src/pages/chat.vue
@@ -2,7 +2,7 @@
 import ActiveChatContainer from "@/views/chat/ActiveChatContainer.vue";
 import MessagesInput from "@/views/chat/MessagesInput.vue";
 import SideBar from "@/views/chat/SideBar.vue";
-import { onMounted, onUnmounted, ref } from "vue";
+import { computed, onMounted, onUnmounted, ref } from "vue";
 
 //1
 const isMobileView = ref<boolean>(false);
@@ -23,17 +23,43 @@ onMounted(() => {
 onUnmounted(() => {
   mediaQuery?.removeEventListener("change", handleWindowRsize);
 });
+
+const showSideBar = computed(() => {
+  if (!isMobileView.value) {
+    // always show the side bar on desktop view
+    return true;
+  } else {
+    // but show it on mobile only if its the active tab
+    return activeView.value === "menu";
+  }
+});
+
+const showActiveChat = computed(() => {
+  if (!isMobileView.value) {
+    return true;
+  } else {
+    return activeView.value === "chat";
+  }
+});
 </script>
 
 <template>
   {{ isMobileView }}
+  <button
+    v-if="isMobileView"
+    @click="
+      activeView === 'chat' ? (activeView = 'menu') : (activeView = 'chat')
+    "
+  >
+    click to switch views on mobile
+  </button>
   <VContainer class="pa-0">
     <div class="py-5" style="height: 100vh">
       <VRow class="h-100 px-2" style="border: 1px solid green">
-        <VCol cols="12" md="4">
+        <VCol cols="12" md="4" v-if="showSideBar">
           <SideBar />
         </VCol>
-        <VCol cols="12" md="8">
+        <VCol cols="12" md="8" v-if="showActiveChat">
           <ActiveChatContainer />
           <MessagesInput />
         </VCol>
--- src/views/chat/messagesComponents/SystemMessage.vue
+++ src/views/chat/messagesComponents/SystemMessage.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div class="text-center text-grey">
+  <div class="text-center text-grey mb-2">
     <!-- to do add the system message here  -->
      user mohanad has joined the chat!
   </div>
